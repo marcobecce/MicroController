d597cd67ce83ce27d15e4904ab0ab1c4fb304191
marcobecce <marco.becce@student.ucll.be>
2026-03-13 00:34:53 +0100

    ...

--- opdracht2/main/main.c
+++ opdracht2/main/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include "myGPIO.h"
+
+#define LED 17
+#define BUTTON 27
 
 void app_main(void)
 {
-
+    myGPIO_LED_Setup(LED);
+    myGPIO_LED_aan(LED);
+    myGPIO_BUTTON_Setup(BUTTON);
+    
+    while(1)
+    {
+        int counter = 0;
+        
+       
+        while(counter < 5)
+        {
+            if(myGPIO_BUTTON_Flank(BUTTON))
+            {
+                counter++;
+            }
+        }
+        
+        
+        myGPIO_LED_Toggle(LED);
+    }
 }
+
